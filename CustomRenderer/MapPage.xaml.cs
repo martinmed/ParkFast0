db1c13a1b12f7940faad084d2e2448b28c3752a6
Martin Medina Nualart <medinanualartmartin@gmail.com>
2017-10-03 02:27:48 -0300

    pines estaticos y se obtiene datos de webservice

--- CustomRenderer/MapPage.xaml.cs
+++ CustomRenderer/MapPage.xaml.cs
@@ -1,47 +1,135 @@
 ﻿using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
+using Plugin.Geolocator;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using System;
+using System.Net;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Xamarin.Forms.Xaml;
+using Xamarin.Auth;
+using Xamarin.Forms.Xaml;
+using System.Net.Http;
 
 namespace CustomRenderer
 {
-	public partial class MapPage : ContentPage
+   
+    public partial class MapPage : ContentPage
 	{
-		public MapPage ()
+        public CustomMap mapa = new CustomMap();
+
+        string datosOcupacion;
+
+        string obtenerDatosOcupacion()
+        {
+            string url = "http://tesis2017.000webhostapp.com/webservice.php";
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = client.GetAsync(url).Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        datosOcupacion = content.ReadAsStringAsync().Result;
+                        return datosOcupacion;
+                    }
+                }
+            }
+        }        
+
+        private async void findMe()
+        {
+            var locator = CrossGeolocator.Current;
+            Plugin.Geolocator.Abstractions.Position position = new Plugin.Geolocator.Abstractions.Position();
+
+            position = await locator.GetPositionAsync();
+            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(position.Latitude, position.Longitude),Distance.FromMiles(1)));
+        }
+
+        private async void requestGPSAsync()
+        {
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    {
+                        await DisplayAlert("Need location", "Gunna need that location", "OK");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+                    //Best practice to always check that the key exists
+                    if (results.ContainsKey(Permission.Location))
+                        status = results[Permission.Location];
+                }
+                if (status == PermissionStatus.Granted)
+                {
+                    var results = await CrossGeolocator.Current.GetPositionAsync();
+                }
+                else if (status != PermissionStatus.Unknown)
+                {
+                    await DisplayAlert("Location Denied", "Can not continue, try again.", "OK");
+                }
+            }
+            catch 
+            {
+
+            }
+        }
+
+        public MapPage ()
 		{
-			InitializeComponent ();
+            
+            InitializeComponent ();
 
-            var pin = new CustomPin
+            //customMap.IsShowingUser = true;
+            //requestGPSAsync();
+            var datosusuario=obtenerDatosOcupacion();
+            var datosResultadoOcupacion = JArray.Parse(datosusuario);
+            //findMe();
+
+            
+
+            customMap.CustomPins = new List<CustomPin>();
+            int contador = 0;
+            string estado = "";
+            Boolean estadoB;
+            foreach (var element in datosResultadoOcupacion)
             {
-                Pin = new Pin
+                if ((int)datosResultadoOcupacion[contador]["estado"]==0)
                 {
-                    Type = PinType.Place,
-                    Position = new Position(37.79752, -121.40183),
-                    Label = "a1",
-                    Address = "1"
-                },
-                Id = "Xamarin",
-                Url = "http://xamarin.com/about/",
-                Estado=true
-            };
-
-            var pin2 = new CustomPin
-            {
-                Pin = new Pin
+                    estadoB = true;
+                    estado = "Ocupado";
+                }
+                else
                 {
-                    Type = PinType.Place,
-                    Position = new Position(36.79752, -121.40183),
-                    Label = "b2",
-                    Address = "2"
-                },
-                Id = "Google",
-                Url = "http://google.com/about/",
-                Estado=false
-            };
-
-            customMap.CustomPins = new List<CustomPin> { pin,pin2 };
-			customMap.Pins.Add (pin.Pin);
-            customMap.Pins.Add (pin2.Pin);
-            customMap.MoveToRegion (MapSpan.FromCenterAndRadius (new Position (37.79752, -122.40183), Distance.FromMiles (1.0)));
-		}
+                    estadoB = false;
+                    estado = "Desocupado";
+                }
+                var pin = new CustomPin
+                {
+                    Pin = new Pin
+                    {
+                        Type = PinType.Place,
+                        Position = new Position((double)
+                        datosResultadoOcupacion[contador]["coordenadas_lat"]
+                        , (double)datosResultadoOcupacion[contador]["coordenadas_lon"]),
+                        Label = "a1",
+                        Address = "En calle " + datosResultadoOcupacion[contador]["calle"].ToString() + " con " + datosResultadoOcupacion[contador]["interseccion1"].ToString(),
+                    },
+                    Id = estado,
+                    Url = "",
+                    Estado = estadoB
+                };
+                customMap.Pins.Add(pin.Pin);
+                contador++;
+            }
+            
+        }
 	}
 }
